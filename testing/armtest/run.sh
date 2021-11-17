61271d835c12bcf0c5c6affbc4d73bd73f72174d
Andrew Cholakian <andrew@andrewvc.com>
2021-11-17 13:01:38 -0600

    Update run.sh

--- testing/armtest/run.sh
+++ testing/armtest/run.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
+# Follow this tutorial https://www.stereolabs.com/docs/docker/building-arm-container-on-x86/
 docker build --platform linux/arm64/v8 -t armexperiment .  
 docker run --rm -it --platform linux/arm64/v8 --volume=$HOME/projects/beats/:/beats armexperiment  /bin/bash -i
